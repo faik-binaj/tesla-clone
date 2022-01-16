93d2ff1a2f7e3d0d4a1d0597417c4a6ed53b7265
fabjo <faik.binaj@gmail.com>
2022-01-16 17:33:01 +0100

    ADD: redux for cars

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,24 +1,33 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
-function Section({ title, description, leftBtnText, rightBtnText, backgroundImg  }) {
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
   return (
     <Wrap bgImage={backgroundImg}>
-      <ItemText>
-        <h1>{title}</h1>
-        <p>{ description }</p>
-      </ItemText>
-        <Buttons>
-        <ButtonGroup>
-            <LeftButton>
-                {leftBtnText}
-            </LeftButton>
-            {rightBtnText && <RightButton>
-                {rightBtnText}
-            </RightButton>}
-        </ButtonGroup>
-        <DownArrow src={'/images/down-arrow.svg'} />
-        </Buttons>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
+
+      <Buttons>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          </ButtonGroup>
+        </Fade>
+
+        <DownArrow src={"/images/down-arrow.svg"} />
+      </Buttons>
     </Wrap>
   );
 }
@@ -35,26 +44,25 @@ const Wrap = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  background-image: ${props => `url("/images/${props.bgImage}")`};
-  
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
 `;
 
 const ItemText = styled.div`
   padding-top: 15vh;
   text-align: center;
+  z-index: -1;
 `;
 
 const ButtonGroup = styled.div`
-    display: flex;
+  display: flex;
   margin-bottom: 30px;
   @media (max-width: 768px) {
     flex-direction: column;
   }
-  
-`
+`;
 
 const LeftButton = styled.div`
-    background-color: rgba(23, 26, 32, 0.8) ;
+  background-color: rgba(23, 26, 32, 0.8);
   height: 40px;
   width: 256px;
   color: white;
@@ -67,21 +75,18 @@ const LeftButton = styled.div`
   font-size: 12px;
   cursor: pointer;
   margin: 8px;
-`
+`;
 
 const RightButton = styled(LeftButton)`
-    background: white;
+  background: white;
   opacity: 0.65;
   color: black;
-`
+`;
 
 const DownArrow = styled.img`
   height: 40px;
   overflow-x: hidden;
   animation: animateDown infinite 1.5s;
-`
+`;
 
-const Buttons = styled.div`
-    
-
-`
+const Buttons = styled.div``;
